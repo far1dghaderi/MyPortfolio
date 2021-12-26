d3be22aaccf6f8533e6700f5e8e77a6fdc9452df
farid ghaderi <farid.51gh@gmail.com>
2021-12-26 18:02:07 +0330

    fix some typos

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -15,13 +15,15 @@ function AboutCard() {
             exam that I was going to took, I just stopped learning programming
             for like 6-8 monthes and after that, I began programming again with{" "}
             <strong className="purple">Javascript</strong> and I'm in love with
-            it till now :D. Because I love to{" "}
+            it till now :D. I really love to{" "}
             <strong className="purple">solve probelms</strong> and mess around
-            with <strong className="purple">data and logical problems </strong>,
-            I chose Back-end for my starting stack, but for sure I will go for
-            being a <strong className="purple">Full-stack</strong> developer in
-            the near future; <strong className="purple">MERN stack</strong> is
-            my favourite.
+            with <strong className="purple">data </strong>and{" "}
+            <strong className="purple">logical problems,</strong>
+            So beacuse of that, I chose Back-end as my starting stack, but for
+            sure I will go for being a{" "}
+            <strong className="purple">Full-stack</strong> developer in the near
+            future; <strong className="purple">MERN stack</strong> is my
+            favourite.
             <br />
             <br />
             As a human, I'm always <strong className="purple">
@@ -35,13 +37,14 @@ function AboutCard() {
             <strong className="purple"> eager to learn</strong> are other
             possitive properties of my personlaity. For sure I'm not a perfect
             person and I do have some negative properties as well, but it
-            totally depends on the environment that I'm working in, if it's
-            good, I'm also good, otherwise, I will swtich to my not-good version
+            totally depends on the environment that I'm working in; if it's
+            good, I'm also good, otherwise I will swtich to my not-good version
             as well.
             <br />
             <br />
             <br />
-            Apart from coding, some other activities that I love to do!
+            Apart from coding, there are some other activities that I love to
+            do:
           </p>
           <ul>
             <li className="about-activity">
